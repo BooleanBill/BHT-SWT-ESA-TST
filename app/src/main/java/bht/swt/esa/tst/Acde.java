5c8a0fbb89c93bae79cfcf43cf218aeafc015131
Martin Klejewski <martin@klejewski.com>
2024-01-14 13:30:55 +0100

    more methods

--- app/src/main/java/bht/swt/esa/tst/Acde.java
+++ app/src/main/java/bht/swt/esa/tst/Acde.java
@@ -8,15 +8,16 @@ public class Acde {
 
     /** a constructor that runs the methods */
     public Acde() {
-        helloWorld(null);
+        helloWorld();
     }
 
     /**
-     * A method that does nothing.
+     * hello world method
+     * s
      * 
      * @param args
      */
-    public static void helloWorld(String[] args) {
+    public static void helloWorld() {
         System.out.println("Hello World!");
     }
 
@@ -25,6 +26,50 @@ public class Acde {
      */
     public String getEmptyString() {
         return "";
+    }
+
+    /**
+     * Adds two integers and returns the result.
+     *
+     * @param a the first integer
+     * @param b the second integer
+     * @return the sum of the two integers
+     */
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    /**
+     * Subtracts two integers.
+     *
+     * @param a the first integer
+     * @param b the second integer
+     * @return the difference of the two integers
+     */
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
+    /**
+     * Multiplies two integers.
+     *
+     * @param a The first integer.
+     * @param b The second integer.
+     * @return The product of the two integers.
+     */
+    public int mul(int a, int b) {
+        return a * b;
+    }
+
+    /**
+     * Divides two floats and returns the result.
+     *
+     * @param a the dividend
+     * @param b the divisor
+     * @return the quotient of the division
+     */
+    public float div(float a, float b) {
+        return a / b;
     }
 
 }
---
+++ app/src/test/java/bht/swt/esa/tst/AcdeTest.java
@@ -0,0 +1,11 @@
+package bht.swt.esa.tst;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Test;
+
+public class AcdeTest {
+
+}
